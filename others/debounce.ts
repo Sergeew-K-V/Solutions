71aaa21d4cfaadba95ee918d31fc2619445c77fc
kirill.sergeev <kirill.sergeev@progforce.com>
2025-02-13 13:05:34 +0300

    debounce

---
+++ others/debounce.ts
@@ -0,0 +1,13 @@
+function debounce<F extends (...args: unknown[]) => unknown>(
+  func: F,
+  wait: number
+): (...args: Parameters<F>) => void {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<F>): void => {
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => func(...args), wait);
+  };
+}
