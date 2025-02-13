0bd86488027988c5c6bd4bdf0a3f2057ae18ff20
kirill.sergeev <kirill.sergeev@progforce.com>
2025-02-13 13:07:13 +0300

    update debounce

--- others/debounce.ts
+++ others/debounce.ts
@@ -1,4 +1,4 @@
-function debounce<F extends (...args: unknown[]) => unknown>(
+function debounce<F extends (...args: any[]) => any>(
   func: F,
   wait: number
 ): (...args: Parameters<F>) => void {
@@ -11,3 +11,36 @@ function debounce<F extends (...args: unknown[]) => unknown>(
     timeout = setTimeout(() => func(...args), wait);
   };
 }
+
+// Пример использования в компоненте
+// const DebounceExample: React.FC = () => {
+//   const [inputValue, setInputValue] = useState<string>('');
+//   const [debouncedValue, setDebouncedValue] = useState<string>('');
+
+//   // Создаем debounced функцию
+//   const debouncedSetDebouncedValue = debounce((value: string) => {
+//     setDebouncedValue(value);
+//   }, 500);
+
+//   // Обрабатываем изменение input
+//   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const value = event.target.value;
+//     setInputValue(value);
+//     debouncedSetDebouncedValue(value);
+//   };
+
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         value={inputValue}
+//         onChange={handleInputChange}
+//         placeholder="Type something..."
+//       />
+//       <p>Input Value: {inputValue}</p>
+//       <p>Debounced Value: {debouncedValue}</p>
+//     </div>
+//   );
+// };
+
+// export default DebounceExample;
